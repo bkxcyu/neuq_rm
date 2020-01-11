6d7842ffed064bd6c899400fa82b49e8c4fa0db1
qqqsy <57007461+QSiY@users.noreply.github.com>
2020-01-11 11:09:17 +0800

    实验
    1

--- down/User/main.c
+++ down/User/main.c
@@ -17,7 +17,7 @@
 #include <string.h>
 #include "gimbal.h"
 
-#define MAX_MOTOR_SPEED   15336				//电机最大转速，宏定义方便修改   范围0 - 10000   15336
+#define MAX_MOTOR_SPEED   15336				//电机最大转速，宏定义方便修改   范围0 - 10000   15336     qsy is sb
 #define MAX_BASE_LINEAR_SPEED    217.817f    //底盘最大平移速度，单位cm/s
 #define MAX_BASE_ROTATIONAL_SPEED    7.260570f    //底盘最大旋转速度，单位rad/s
 
