9d09fa4a7d584a9af50393147877aa007fc0c65e
dzw <931160477@qq.com>
2020-01-16 04:33:40 -0800

    rviz

--- up/zhidao/src/tf2.py
+++ up/zhidao/src/tf2.py
@@ -7,7 +7,7 @@ import math
 from geometry_msgs.msg import Twist
 from visualization_msgs.msg import *
 from geometry_msgs.msg import Point
-
+#from sympy import *
 current_time=0
 last_time=0
 delta_time=0
@@ -68,14 +68,36 @@ if __name__=="__main__":
 	rospy.init_node('my_tf_broadcaster')
 	rospy.Subscriber('cmd_vel', Twist, callback)
 	topic = 'visualization_marker_array'
-	topic2 = 'visualization_marker_line'
+	#topic2 = 'visualization_marker_line'
+	topic3 = 'draw_bullet_tracks'
+	topic4 = 'draw_target_points'
 	publisher = rospy.Publisher(topic, MarkerArray,queue_size = 100)
 	markerArray = MarkerArray()
 	#publisherline = rospy.Publisher(topic2, Marker,queue_size = 100)
-	#count = 0
-	#MARKERS_MAX = 100
+	publisherpoint = rospy.Publisher(topic4, Marker,queue_size = 100)
+	count = 0
+	MARKERS_MAX = 100
 	rospy.Subscriber("point3d", Point, positioncallback)
 	while not rospy.is_shutdown():
+		marker1 = Marker()
+		marker1.header.frame_id = "/world"
+		marker1.type = Marker.CUBE
+		marker1.action = Marker.ADD
+		marker1.ns = 'Testline'
+		marker1.scale.x = 0.5
+		marker1.scale.y = 0.5
+		marker1.scale.z = 0.5
+		marker1.color.a = 1.0
+		marker1.color.r = 1.0
+		marker1.color.g = 1.0
+		marker1.color.b = 0.0
+		marker1.pose.orientation.w = 1.0
+		marker1.pose.position.x = position_x
+		marker1.pose.position.y = position_y
+		marker1.pose.position.z = position_z
+		marker1.id=0
+		publisherpoint.publish(marker1)
+		'''
 		##the position of target
 		marker = Marker()
 		marker.header.frame_id = "/world"
@@ -105,8 +127,7 @@ if __name__=="__main__":
 		# Publish the MarkerArray
 		publisher.publish(markerArray)
 		#count += 1
-
-		
+		'''
 		#draw square lines hpljy
 		#line_list = Marker()
 		#line_list.header.frame_id = "/world"
@@ -166,15 +187,21 @@ if __name__=="__main__":
 		ninea_y=position_y-y
 		ninea_x=position_x-x
 		ninea_z=position_z-1
-		rospy.loginfo("nine_y%f",ninea_y)
-		theta=math.atan((ninea_y)/(ninea_x))
+		#rospy.loginfo("nine_y%f",ninea_y)
+		pitch1=math.atan((ninea_y)/(ninea_x))
 		if ninea_x<0 and ninea_y>0:
-			theta = 3.1415926 + math.atan((ninea_y)/(ninea_x))
+			pitch1 = 3.1415926 + math.atan((ninea_y)/(ninea_x))
 		if ninea_x<0 and ninea_y<0:
-			theta = 3.1415926 + math.atan((ninea_y)/(ninea_x))
+			pitch1 = 3.1415926 + math.atan((ninea_y)/(ninea_x))
 		if ninea_x>0 and ninea_y<0:
-			theta =  math.atan((ninea_y)/(ninea_x))
-		pitch =-math.atan((ninea_z)/math.sqrt(ninea_x*ninea_x+ninea_y*ninea_y))
+			pitch1 =  math.atan((ninea_y)/(ninea_x))
+		#yaw =-math.atan((ninea_z)/math.sqrt(ninea_x*ninea_x+ninea_y*ninea_y))-0.1
+		s=math.sqrt(position_x*position_x+position_y*position_y)
+		v=10
+  		yaw=-math.atan((2*math.pow(v,2)*s-math.sqrt(4*math.pow(v,4)*math.pow(s,2)-8*9.8*math.pow(v,2)*math.pow(s,2)*position_z-4*9.8*9.8*math.pow(s,4)))/(2*9.8*math.pow(s,2)))+0.1	
+		#fai=symbol('fai')
+		#t=symbol('t')
+		#a=(solve())
 		#if ninea_y<0 and ninea_z>0 and ninea_x>0:
 			#pitch =  -math.atan((ninea_z)/(math.sqrt((ninea_x*ninea_x)+(ninea_y*ninea_y))))
 		#if ninea_x<0 and ninea_y>0:
@@ -185,12 +212,12 @@ if __name__=="__main__":
 			#rospy.loginfo("ninea_zsa%f",ninea_z)
 			#rospy.loginfo("ninea_yas%f",ninea_y)
 			#rospy.loginfo("ninea_xas%f",ninea_x)
-		rospy.loginfo("sasdasd%f",theta)
-			#rospy.loginfo("%f",delta_time)
+		#rospy.loginfo("sasdasd%f",theta)
+		#rospy.loginfo("%f",delta_time)
 			#rospy.loginfo("%f",theta)
 			#rospy.loginfo("X%f",x)
 		br1.sendTransform((x,y,1.0),
-						tf.transformations.quaternion_from_euler(0, pitch,theta),
+						tf.transformations.quaternion_from_euler(0, yaw,pitch1),
 						rospy.Time.now(),
 						"gimbal",
 						"world")
@@ -199,5 +226,39 @@ if __name__=="__main__":
 						rospy.Time.now(),
 						"chassis",
 						"world")
+		marker = Marker()
+  		marker.header.frame_id = "/world"
+		marker.type = marker.SPHERE
+		marker.action = marker.ADD
+		marker.scale.x = 0.1
+		marker.scale.y = 0.1
+		marker.scale.z = 0.1
+		marker.color.a = 1.0
+		marker.color.r = 1.0
+		marker.color.g = 1.0
+		marker.color.b = 0.0
+		marker.pose.orientation.w = 1.0
+		if(count>1):
+			count=0
+		#if(count==0):
+		marker.pose.position.x = v*math.cos(yaw)*(1-math.pow(2.71828,-count))*math.cos(pitch1)+x
+		marker.pose.position.y = v*math.cos(yaw)*(1-math.pow(2.71828,-count))*math.sin(pitch1)+y
+		marker.pose.position.z = (9.8+v*math.sin(-yaw))*(1-math.pow(2.71828,-count))-9.8*count+1
+			#a=x
+			#b=y
+		#elif(count!=0):
+			#marker.pose.position.x = v*math.cos(yaw)*(1-math.pow(2.71828,-count))*math.cos(pitch1)+a
+			#marker.pose.position.y = v*math.cos(yaw)*(1-math.pow(2.71828,-count))*math.sin(pitch1)+b
+			#marker.pose.position.z = (9.8+v*math.sin(-yaw))*(1-math.pow(2.71828,-count))-9.8*count+1
+		if(count > 0.1):
+			markerArray.markers.pop(0)
+
+		markerArray.markers.append(marker)
+		id = 1
+		for m in markerArray.markers:
+			m.id = id
+			id += 1
+		publisher.publish(markerArray)
+		count += 0.01
 		rospy.sleep(0.01)
 
