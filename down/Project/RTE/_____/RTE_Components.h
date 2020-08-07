93393fdde175038ba4cf27f349bd48747ba11e5a
qqqsy <57007461+QSiY@users.noreply.github.com>
2020-08-07 08:55:58 +0800

    111
    11

---
+++ down/Project/RTE/_____/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'RM-F427' 
+ * Target:  '╣вел' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f4xx.h"
+
+#define JANSSON
+
+#endif /* RTE_COMPONENTS_H */
